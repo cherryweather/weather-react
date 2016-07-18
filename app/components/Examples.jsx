837a6879a18748f271b83402275af65ed7ca4097
unknown <jzxc2012@gmail.com>
2016-07-18 23:17:04 +0300

    Add examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+import { Link } from 'react-router';
 
 var Examples = (props) => {
   return (
-    <h4>Examples</h4>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are some examples</p>
+      <ol>
+        <li>
+          <Link to='/?location=Austin'>Austin</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio</Link>
+        </li>
+      </ol>
+    </div>
   );
 };
 
